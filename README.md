3bdd03d6f9f0e840c8017382e770348fe4160d92
tamectosphere <tam.ectosphere@gmail.com>
2024-02-18 11:33:57 +0700

    feat(util-scripts): add clone-github script

--- README.md
+++ README.md
@@ -9,11 +9,26 @@ This repository contains a collection of utility scripts designed to automate va
 ### 1. [InitGithub](init-github.py)
 
 #### Description
+
 The `InitGithub` script automates the initialization of a Git repository with specific configurations. It takes input parameters such as the host, repository path, repository name, Github user email, and Github user name. The script executes a series of Git commands, including initializing a new Git repository, adding a remote origin, creating a README file with a header based on the provided repository name, and staging the README file for the initial commit.
 
 ### Usage
 
 To initialize a new GitHub repository, use the following command:
 
-```bash
+````bash
 python init-github.py <path> '{"host": "github.com", "repo_path": "marcus.a/example.git", "repo_name": "example", "email": "marcus.a@example.com", "name": "Marcus Aurelius"}'
+
+### 2. [CloneGithub](clone-github.py)
+
+#### Description
+The `CloneGithub` script automates the cloning of a Git repository with specific configurations. It takes input parameters such as the host, repository path, Github user email, and Github user name. The script executes a series of Git commands, including cloning a current or forked Git repository, and adding email nad name to config.
+
+### Usage
+
+To clone a GitHub repository, use the following command:
+
+```bash
+python clone-github.py <path> '{"host": "github.com", "repo_path": "marcus.a/example.git", "email": "marcus.a@example.com", "name": "Marcus Aurelius"}'
+
+````
---
+++ clone-github.py
@@ -0,0 +1,72 @@
+import json
+import os
+import subprocess
+import sys
+
+def wait_for_enter():
+    input("Press Enter to continue...")
+
+def run_command(command):
+    print(f"Running command: {command}")
+
+    try:
+        subprocess.run(command, check=True, shell=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Error running command: {e}")
+        print(f"Command output: {e.output}")
+
+class CloneGithub:
+    def run(self, context):
+        host = context.get("host")
+        repo_path = context.get("repo_path")
+
+        run_command("git init")
+        run_command(f"git clone git@{host}:{repo_path}") 
+
+        wait_for_enter()
+
+class ConfigGithubUser:
+    def run(self, context):
+        email = context.get("email")
+        name = context.get("name")
+
+        run_command(f"git config user.email {email}")
+        run_command(f"git config user.name {name}")
+
+
+def get_context(context_str):
+    try:
+        return json.loads(context_str)
+    except json.JSONDecodeError as e:
+        print(f"Invalid JSON: {e}")
+        sys.exit(1)
+
+def main(context_str):
+    context = get_context(context_str)
+
+    procedure = [CloneGithub, ConfigGithubUser]
+
+    for step in procedure:
+        step().run(context)
+
+    print("Done!")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print(
+            'Usage: clone-github.py <path> \'{"host": "github.com", "repo_path": "marcus.a/example.git", "email": "marcus.a@example.com", "name": "Marcus Aurelius"}\''
+        )
+        sys.exit(1)
+
+    cwd = os.getcwd()
+    path = sys.argv[1]
+
+    print(f"{cwd}/{path}")
+
+    try:
+        os.makedirs(f"{cwd}/{path}")
+        os.chdir(f"{cwd}/{path}")
+        main(sys.argv[2])
+
+    except OSError as e:
+        print(f"Error making or changing directory: {e}")
